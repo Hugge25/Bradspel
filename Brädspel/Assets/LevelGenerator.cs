67666442864846a28f210fab4eeb9be962b2d146
Hugge25 <hugo.paulsson@hotmail.com>
2024-01-24 12:21:11 +0100

    bradspel

---
+++ Brädspel/Assets/LevelGenerator.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+
+    public Texture2D map;
+    public ColorToPrefab[] colorMappings;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateLevel1();
+    }
+
+    void GenerateLevel1()
+    {
+        for(int x = 0; x < map.width; x++)
+        {
+            for(int y = 0; y < map.height; y++)
+            {
+                GenerateTile(x, y);
+            }
+        }
+    }
+    
+    void GenerateTile(int x, int y)
+    {
+        Color pixelColor = map.GetPixel(x, y);
+
+        if(pixelColor.a == 0)
+        {
+            //The pixel is transparrent. Let's igonre it
+            return;
+        }
+
+        foreach (ColorToPrefab colorMapping in colorMappings){
+            if(colorMapping.color.Equals(pixelColor))
+            {
+                Vector2 position = new Vector2(x-32, y-30);
+                Instantiate(colorMapping.prefab, position, Quaternion.identity, transform);
+            }
+        }
+    }
+}
