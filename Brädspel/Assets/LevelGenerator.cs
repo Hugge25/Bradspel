a7a659acfbd1e3b3890ce07f3a472fe2e5c0d48b
Hugge25 <hugo.paulsson@hotmail.com>
2024-02-07 12:22:48 +0100

    asdf

--- Brädspel/Assets/LevelGenerator.cs
+++ Brädspel/Assets/LevelGenerator.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
+using TMPro;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class LevelGenerator : MonoBehaviour
 {
-
     public Texture2D map;
     public ColorToPrefab[] colorMappings;
+
+    int gw = 10;
+    int gh = 10;
+    int w = 10;
+    int h = 5;
 
     
     // Start is called before the first frame update
@@ -23,37 +30,47 @@ public class LevelGenerator : MonoBehaviour
 
     void GenerateLevel1()
     {
-        /*for(int x = 0; x < map.width; x++)
-        {
-            for(int y = 0; y < map.height; y++)
-            {
-                GenerateTile(x, y);
-            }
-        }*/
-
         for(int y = 0; y < map.height; y++)
         {
             for(int x = 0; x < map.width; x++)
             {
-                GenerateTile(x, y);
-             }
+                int xPos = x;
+                if(y % 2 == 1) {
+                    xPos = map.width - x;
+                }
+                int num = y * map.width + x;
+                GenerateTile(xPos, y, num);
+            }
          }
     }
-    void GenerateTile(int x, int y)
+    void GenerateTile(int x, int y,int tileNum)
     {
         Color pixelColor = map.GetPixel(x, y);
+        print(pixelColor);
 
         if(pixelColor.a == 0)
         {
             //The pixel is transparrent. Let's igonre it
-            return;
+            //return;
         }
 
         foreach (ColorToPrefab colorMapping in colorMappings){
+            print(colorMapping.color.Equals(pixelColor));
             if(colorMapping.color.Equals(pixelColor))
             {
-                Vector2 position = new Vector2(x-32, y-30);
-                Instantiate(colorMapping.prefab, position, Quaternion.identity, transform);
+                Vector2 position = new Vector2((x*2)-32, (y*8)-50);
+                //Instantiate(colorMapping.prefab, position, Quaternion.identity, transform).GetComponentInChildren<TMP_Text>()
+                GameObject box = Instantiate(colorMapping.prefab, position, Quaternion.identity, transform); 
+                box.GetComponentsInChildren<SpriteRenderer>()[1].flipX = true;
+                TMP_Text text = box.GetComponent<TMP_Text>();
+                box.GetComponent<SpriteRenderer>().color = Color.white;
+                if(tileNum%2==1)
+                {
+                    print(tileNum);
+                    box.GetComponent<SpriteRenderer>().color = Color.red;
+                }
+                if(text != null)
+                    text.text = tileNum.ToString();
             }
         }
     }
