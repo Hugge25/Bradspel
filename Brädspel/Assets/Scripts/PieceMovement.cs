54fbb1be0c9a5f1fb8926ad36f115b0101aeaa53
Hugge25 <hugo.paulsson@hotmail.com>
2024-02-28 18:53:50 +0100

    finito

--- Brädspel/Assets/Scripts/PieceMovement.cs
+++ Brädspel/Assets/Scripts/PieceMovement.cs
@@ -1,20 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class PieceMovement : MonoBehaviour
 {
     public PieceSelecter pieceSelecter;
 
-    public LevelGenerator levelGenerator;
+    public GameObject Gs1;
+    public GameObject Ht1;
+    public GameObject Kn1;
+    public GameObject Hd1;
+    public GameObject Gs2;
+    public GameObject Ht2;
+    public GameObject Kn2;
+    public GameObject Hd2;
 
+    public void Start(){
 
+    }
     public void Update()
     {
         if(pieceSelecter.p1 == 1)
         {
-            
+            Gs1.SetActive(true);
+        }
+        else if(pieceSelecter.p1 == 2){
+            Ht1.SetActive(true);
         } 
+
+        if(pieceSelecter.p2 == 1)
+        {
+            Gs2.SetActive(true);
+        }
+        else if(pieceSelecter.p2 == 2){
+            Ht2.SetActive(true);
+        }   
     }
 }
