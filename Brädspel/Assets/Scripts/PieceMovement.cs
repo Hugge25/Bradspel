61a170ca7b6f19603bfc432a456300eb570f0dd9
Hugge25§ <hugo.paulsson@hotmail.com>
2024-04-03 12:11:44 +0200

    ez

--- Brädspel/Assets/Scripts/PieceMovement.cs
+++ Brädspel/Assets/Scripts/PieceMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
+using JetBrains.Annotations;
 using TMPro;
 using Unity.Mathematics;
 using UnityEditor;
@@ -19,8 +20,9 @@ public class PieceMovement : MonoBehaviour
     public GameObject prefabStar, prefabBomb;
     public int p1Pos = 0;
     public int p2Pos = 0;
-    private bool p1Finish = false;
-    private bool p2Finish = false;
+    public bool p1Finish = false;
+    public bool p2Finish = false;
+    private bool isWaiting = false;
     public TMP_Text tmp;
 
     private PlayState playState = PlayState.Player1WaitForMove;
@@ -39,6 +41,15 @@ public class PieceMovement : MonoBehaviour
         GameObject star2 = Instantiate(prefabStar, LevelGenerator.tiles[21].transform.position, Quaternion.identity, transform);
         GameObject star3 = Instantiate(prefabStar, LevelGenerator.tiles[37].transform.position, Quaternion.identity, transform);
         GameObject star4 = Instantiate(prefabStar, LevelGenerator.tiles[45].transform.position, Quaternion.identity, transform);
+
+        bomb1.transform.position += Vector3.back;
+        bomb2.transform.position += Vector3.back;
+        bomb3.transform.position += Vector3.back;
+        bomb4.transform.position += Vector3.back;
+        star1.transform.position += Vector3.back;
+        star2.transform.position += Vector3.back;
+        star3.transform.position += Vector3.back;
+        star4.transform.position += Vector3.back;
         
     }   
 
@@ -47,48 +58,74 @@ public class PieceMovement : MonoBehaviour
         p1Pos = Mathf.Min(p1Pos, 61);
         p2Pos = Mathf.Min(p2Pos, 61);
 
-        if((Player1.transform.position == LevelGenerator.tiles[6].transform.position) || 
+        if(((Player1.transform.position == LevelGenerator.tiles[6].transform.position) || 
             (Player1.transform.position == LevelGenerator.tiles[20].transform.position) || 
             (Player1.transform.position == LevelGenerator.tiles[40].transform.position)|| 
-            (Player1.transform.position == LevelGenerator.tiles[56].transform.position))
+            (Player1.transform.position == LevelGenerator.tiles[56].transform.position)) && !isWaiting)
         {
+            StartCoroutine(Wait());
             print("BombP1");
-            Player1.transform.position = LevelGenerator.tiles[0].transform.position;
             p1Pos = 0;
+            Player1.transform.position = LevelGenerator.tiles[p1Pos].transform.position;
         }
 
-       if((Player2.transform.position == LevelGenerator.tiles[6].transform.position) || 
+       if(((Player2.transform.position == LevelGenerator.tiles[6].transform.position) || 
             (Player2.transform.position == LevelGenerator.tiles[20].transform.position) || 
             (Player2.transform.position == LevelGenerator.tiles[40].transform.position)|| 
-            (Player2.transform.position == LevelGenerator.tiles[56].transform.position))
+            (Player2.transform.position == LevelGenerator.tiles[56].transform.position)) && !isWaiting)
         {
+            StartCoroutine(Wait());
             print("BombP2");
-            Player2.transform.position = LevelGenerator.tiles[0].transform.position; 
-            p1Pos = 0;
+            p2Pos = 0;
+            Player2.transform.position = LevelGenerator.tiles[p2Pos].transform.position; 
+            
         }
+
+
+        if(((Player1.transform.position == LevelGenerator.tiles[3].transform.position) || 
+            (Player1.transform.position == LevelGenerator.tiles[21].transform.position) || 
+            (Player1.transform.position == LevelGenerator.tiles[37].transform.position)|| 
+            (Player1.transform.position == LevelGenerator.tiles[45].transform.position)) && !isWaiting)
+        {
+            StartCoroutine(Wait());
+            p1Pos += 2;
+            Player1.transform.position = LevelGenerator.tiles[p1Pos].transform.position; 
+        }
+
+        if(((Player2.transform.position == LevelGenerator.tiles[3].transform.position) || 
+            (Player2.transform.position == LevelGenerator.tiles[21].transform.position) || 
+            (Player2.transform.position == LevelGenerator.tiles[37].transform.position)|| 
+            (Player2.transform.position == LevelGenerator.tiles[45].transform.position)) && !isWaiting)
+        {
+            StartCoroutine(Wait());
+            p2Pos += 2;
+            Player2.transform.position = LevelGenerator.tiles[p2Pos].transform.position; 
+        }
+
+
 
         if(playState == PlayState.Player1WaitForMove){
             tmp.text = "Spelare 1's Tur"; 
         }
-        else if(playState == PlayState.Player1Moving ){
+        else if(playState == PlayState.Player1Moving && !isWaiting){
+            StartCoroutine(Wait());
             MovePlayer1();
         }
         else if(playState == PlayState.Player2WaitForMove){
             tmp.text = "Spelare 2's Tur";
         }
-        else if(playState == PlayState.Player2Moving ){
+        else if(playState == PlayState.Player2Moving && !isWaiting){
+            StartCoroutine(Wait());
             MovePlayer2();
         }
 
-        
         if(p1Pos == 62){
-            p1Finish = true;
+            p1Finish = true; 
         }
+
         if(p2Pos == 62){
             p2Finish = true;
         }
-
-
     }
 
     void MovePlayer1(){
@@ -114,5 +151,12 @@ public class PieceMovement : MonoBehaviour
                 playState = PlayState.Player2Moving;
                 print("Switch to p2");
             }
+    }
+
+    private IEnumerator Wait()
+    {
+        isWaiting = true;
+        yield return new WaitForSeconds(1f);
+        isWaiting = false;
     }
 }
